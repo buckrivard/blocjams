89406f838de3703662351b12ab2df55cb2d6f5a4
Buck Rivard <buckrivard@gmail.com>
2016-12-10 09:40:08 -0800

    checkpoint 9 assignment completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -10,9 +10,7 @@ var animatePoints = function(points) {
         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
                     
-    for (var i = 0; i < points.length; i++) {
-                    revealPoint(i);
-    }
+    points.forEach(revealPoint); 
 };
 
 window.onload = function() {
---
+++ scripts/utilities.js
@@ -0,0 +1,5 @@
+function forEach(points, function () {
+    for (var i = 0; i < points.length; i++) {
+        function(points[i]);
+    }
+})
