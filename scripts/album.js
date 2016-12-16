525ddece7dd72b85be0af0e6415d48d75251cc23
Buck Rivard <buckrivard@gmail.com>
2016-12-16 10:13:22 -0800

    assignment function refactored

--- scripts/album.js
+++ scripts/album.js
@@ -65,15 +65,12 @@ var setCurrentAlbum = function (album) {
 var findParentByClassName = function (element, targetClass) {
     if (element) {
         var parEl = element.parentElement;
-        while (parEl.className !== targetClass && parEl.className !== null) {
+        if (!parEl) {
+            return alert("No parent found.");
+        while (parEl && parEl.className !== targetClass && parEl.className !== null) {
             parEl = parEl.parentElement;
         }
-        if (!parEl) {
-            alert("No parent found.");
-        } else if (parEl.className !== targetClass) {
-            alert("No parent found with that class name")
-        }
-        return parEl;
+        return parEl ? parEl : alert("No parent found with that class name")
     } 
 }
 
